ef5636bc68e1520bc5fdc1d65419cb3d64ee1495
jasonxyliu <xinyu_liu@brown.edu>
2023-02-12 18:25:37 -0500

    updated: env name, args

--- src/run_train_agent.sh
+++ src/run_train_agent.sh
@@ -8,10 +8,10 @@
 #SBATCH -e sbatch_out/arrayjob-%A-%a.err
 #SBATCH -o sbatch_out/arrayjob-%A-%a.out
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/users/xliu53/anaconda/ltl2action/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/users/xliu53/anaconda/ltl_transfer_v1/lib
 
 module load anaconda/2022.05
 source /gpfs/runtime/opt/anaconda/2022.05/etc/profile.d/conda.sh
-conda activate ltl2action
+conda activate ltl_transfer_v1
 
-python train_agent.py --algo ppo --env Simple-LTL-Env-v0 --ltl-sampler Transfer_train_mixed_50 --log-interval 1 --save-interval 30 --frames 10000000 --epochs 2 --batch-size 1024 --frames-per-proc 512 --discount 0.9 --lr 0.001 --gae-lambda 0.5 --clip-eps 0.1 --dumb-ac
+python train_agent.py --algo ppo --env Simple-LTL-Env-v0 --ltl-sampler Transfer_train_hard_50 --log-interval 1 --save-interval 30 --procs 1 --frames 10000000 --epochs 2 --batch-size 1024 --frames-per-proc 512 --discount 0.9 --lr 0.001 --gae-lambda 0.5 --clip-eps 0.1 --dumb-ac
